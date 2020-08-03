437c644a3b5ef2abf602b6a6abab82452340aa76
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-03 22:03:36 +0100

    Fixing H10 error

--- server/index.js
+++ server/index.js
@@ -8,16 +8,16 @@ const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
 
-const mongoose = require("mongoose");
-mongoose
-  .connect(config.mongoURI, { useNewUrlParser: true })
-  .then(() => console.log("DB connected"))
-  .catch(err => console.error(err));
-
 // const mongoose = require("mongoose");
-// const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB Connected...'))
-//   .catch(err => console.log(err));
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI.toString(), { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.use(cors())
 
