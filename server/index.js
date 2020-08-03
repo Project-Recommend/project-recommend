c3a8bfbd341c434f2a1032b32de880c75779c7c8
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-03 23:14:42 +0100

    test

--- server/index.js
+++ server/index.js
@@ -38,11 +38,11 @@ app.use('/uploads', express.static('uploads'));
 if (process.env.NODE_ENV === "production") {
 
   // Set static folder
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
