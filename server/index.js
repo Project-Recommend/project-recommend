465e8a8f0030abc7aa34ab872e6ec60f637166d7
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-03 22:18:33 +0100

    Updt

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const config = require("./config/key");
 //   .catch(err => console.error(err));
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI.toString(), { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
