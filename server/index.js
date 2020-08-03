75bd6fb06d0b2b8b8370341beb592bf6cc0f1180
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-03 22:28:23 +0100

    Testing bug fix

--- server/index.js
+++ server/index.js
@@ -15,9 +15,10 @@ const config = require("./config/key");
 //   .catch(err => console.error(err));
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+const connect = mongoose
+    .connect("mongodb+srv://dsango:V3nu5266£££@projectrecommenddb.ywrnj.mongodb.net/testDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
 
 app.use(cors())
 
