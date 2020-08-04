f7b8fcdc814e2246cd89c81c589b83cbcf0e1f25
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-04 16:47:01 +0100

    Trying something

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();
+// const express() = express();
 const path = require("path");
 const cors = require('cors')
 
@@ -20,34 +20,34 @@ const connect = mongoose
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.log(err));
 
-app.use(cors())
+express().use(cors())
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
+express().use(bodyParser.urlencoded({ extended: true }));
+express().use(bodyParser.json());
+express().use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/product', require('./routes/product'));
+express().use('/api/users', require('./routes/users'));
+express().use('/api/product', require('./routes/product'));
 
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use('/uploads', express.static('uploads'));
+express().use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 
   // Set static folder
-  app.use(express.static("client/build"));
+  express().use(express.static("client/build"));
 
   // index.html for all page routes
-  app.get("*", (req, res) => {
+  express().get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
+express().listen(port, () => {
   console.log(`Server Running at ${port}`)
 });
