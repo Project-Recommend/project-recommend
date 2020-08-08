b0dbb50d0b4f0980ab981551b59bfbe5fd47ffaf
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-08 01:03:29 +0100

    Formatting

--- client/src/components/views/CatalogPage/CatalogPage.js
+++ client/src/components/views/CatalogPage/CatalogPage.js
@@ -1,53 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import Axios from 'axios';
-import { Icon, Col, Card, Row } from 'antd';
-// import ImageSlider from '../../utils/ImageSlider';
-import CheckBox from './Sections/CheckBox';
-import RadioBox from './Sections/RadioBox';
-import { skilllevel, participation } from './Sections/Datas';
-import SearchFeature from './Sections/SearchFeature';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import CheckBox from "./Sections/CheckBox";
+import RadioBox from "./Sections/RadioBox";
+import { skilllevel, participation } from "./Sections/Datas";
+import SearchFeature from "./Sections/SearchFeature";
+import { Icon, Col, Card, Row } from "antd";
 
 const { Meta } = Card;
 
 function LandingPage() {
-
-    const [Products, setProducts] = useState([])
-    const [Skip, setSkip] = useState(0)
-    const [Limit, setLimit] = useState(8)
-    const [PostSize, setPostSize] = useState()
-    const [SearchTerms, setSearchTerms] = useState("")
+    const [Products, setProducts] = useState([]);
+    const [Skip, setSkip] = useState(0);
+    const [Limit, setLimit] = useState(8);
+    const [PostSize, setPostSize] = useState();
+    const [SearchTerms, setSearchTerms] = useState("");
 
     const [Filters, setFilters] = useState({
         skilllevel: [],
-        participation: []
-    })
+        participation: [],
+    });
 
     useEffect(() => {
-
         const variables = {
             skip: Skip,
             limit: Limit,
-        }
+        };
 
-        getProducts(variables)
-
-    }, [])
+        getProducts(variables);
+    }, []);
 
     const getProducts = (variables) => {
-        Axios.post('/api/product/getProducts', variables)
-            .then(response => {
-                if (response.data.success) {
-                    if (variables.loadMore) {
-                        setProducts([...Products, ...response.data.products])
-                    } else {
-                        setProducts(response.data.products)
-                    }
-                    setPostSize(response.data.postSize)
+        Axios.post("/api/product/getProducts", variables).then((response) => {
+            if (response.data.success) {
+                if (variables.loadMore) {
+                    setProducts([...Products, ...response.data.products]);
                 } else {
-                    alert('Failed to fectch product datas')
+                    setProducts(response.data.products);
                 }
-            })
-    }
+                setPostSize(response.data.postSize);
+            } else {
+                alert("Failed to fectch product datas");
+            }
+        });
+    };
 
     const onLoadMore = () => {
         let skip = Skip + Limit;
@@ -57,130 +52,108 @@ function LandingPage() {
             limit: Limit,
             loadMore: true,
             filters: Filters,
-            searchTerm: SearchTerms
-        }
-        getProducts(variables)
-        setSkip(skip)
-    }
-
+            searchTerm: SearchTerms,
+        };
+        getProducts(variables);
+        setSkip(skip);
+    };
 
     const renderCards = Products.map((product, index) => {
-
         return (
             <Col lg={6} md={8} xs={24}>
-                <Card
-                    hoverable={true}
-                >
-                    <Meta title={product.title} description={<a href={`/product/${product._id}`} style={{textDecoration: "none"}}>{product.description}</a>}></Meta>
+                <Card hoverable={true}>
+                    <Meta
+                        title={product.title}
+                        description={
+                            <a href={`/product/${product._id}`} style={{ textDecoration: "none" }}>
+                                {product.description}
+                            </a>
+                        }
+                    ></Meta>
                 </Card>
             </Col>
         );
-    })
-
+    });
 
     const showFilteredResults = (filters) => {
-
         const variables = {
             skip: 0,
             limit: Limit,
-            filters: filters
-
-        }
-        getProducts(variables)
-        setSkip(0)
-
-    }
+            filters: filters,
+        };
+        getProducts(variables);
+        setSkip(0);
+    };
 
     const handleFilters = (filters, category) => {
+        const newFilters = { ...Filters };
 
-        const newFilters = { ...Filters }
+        newFilters[category] = filters;
 
-        newFilters[category] = filters
+        console.log(newFilters);
 
-        console.log(newFilters)
-
-        showFilteredResults(newFilters)
-        setFilters(newFilters)
-    }
+        showFilteredResults(newFilters);
+        setFilters(newFilters);
+    };
 
     const updateSearchTerms = (newSearchTerm) => {
-
         const variables = {
             skip: 0,
             limit: Limit,
             filters: Filters,
-            searchTerm: newSearchTerm
-        }
+            searchTerm: newSearchTerm,
+        };
 
-        setSkip(0)
-        setSearchTerms(newSearchTerm)
+        setSkip(0);
+        setSearchTerms(newSearchTerm);
 
-        getProducts(variables)
-    }
-
+        getProducts(variables);
+    };
 
     return (
-        <div style={{ width: '75%', margin: '3rem auto' }}>
-            <div style={{ textAlign: 'center' }}>
-                <h1>  Catalog  <Icon type="rocket" />  </h1>
+        <div style={{ width: "75%", margin: "3rem auto" }}>
+            <div style={{ textAlign: "center" }}>
+                <h1>
+                    {" "}
+                    Catalog <Icon type="rocket" />{" "}
+                </h1>
             </div>
-
 
             {/* Filter  */}
 
             <Row gutter={[16, 16]}>
-                <Col lg={12} xs={24} >
-                    <CheckBox
-                        title={"Skill Level"}
-                        list={skilllevel}
-                        handleFilters={filters => handleFilters(filters, "skilllevel")}
-                    />
+                <Col lg={12} xs={24}>
+                    <CheckBox title={"Skill Level"} list={skilllevel} handleFilters={(filters) => handleFilters(filters, "skilllevel")} />
                 </Col>
                 <Col lg={12} xs={24}>
-                    <CheckBox
-                        title={"Participation"}
-                        list={participation}
-                        handleFilters={filters => handleFilters(filters, "participation")}
-                    />
+                    <CheckBox title={"Participation"} list={participation} handleFilters={(filters) => handleFilters(filters, "participation")} />
                 </Col>
             </Row>
 
-
             {/* Search  */}
-            <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '1rem auto' }}>
-
-                <SearchFeature
-                    refreshFunction={updateSearchTerms}
-                />
-
+            <div style={{ display: "flex", justifyContent: "flex-end", margin: "1rem auto" }}>
+                <SearchFeature refreshFunction={updateSearchTerms} />
             </div>
 
-
-            {Products.length === 0 ?
-                <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+            {Products.length === 0 ? (
+                <div style={{ display: "flex", height: "300px", justifyContent: "center", alignItems: "center" }}>
                     <h2>No projects yet...</h2>
-                </div> :
-                <div>
-                    <Row gutter={[16, 16]}>
-
-                        {renderCards}
-
-                    </Row>
-
-
                 </div>
-            }
-            <br /><br />
+            ) : (
+                <div>
+                    <Row gutter={[16, 16]}>{renderCards}</Row>
+                </div>
+            )}
+            <br />
+            <br />
 
-            {PostSize >= Limit &&
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {PostSize >= Limit && (
+                <div style={{ display: "flex", justifyContent: "center" }}>
                     <button onClick={onLoadMore}>Load More</button>
                 </div>
-            }
-
-
+            )}
         </div>
-    )
+    );
 }
 
-export default LandingPage
+export default LandingPage;
