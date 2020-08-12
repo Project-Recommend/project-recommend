0da2f928ad1b8bd4a383e068da2d17fe565b7919
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-13 00:09:59 +0100

    Corrected link for linkedin icon

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <section className="footer">
             <div className="container">
                 <div className="footer-logo">Project Recommend</div>
-                <a href="https://github.com/project-recommend"><Icon type="linkedin" className="footer-links footer-icons" /></a><a href="mailto:projectrecommend@gmail.com"><Icon type="mail" className="footer-links footer-icons" /></a>
+                <a href="https://linkedin.com/company/project-recommend/"><Icon type="linkedin" className="footer-links footer-icons" /></a><a href="mailto:projectrecommend@gmail.com"><Icon type="mail" className="footer-links footer-icons" /></a>
                 <p>© 2020. All rights reserved</p>
             </div>
         </section>
