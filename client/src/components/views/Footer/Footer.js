4e73445bc57e291290e4c3c1a3cbe4379d8861ae
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-08-12 22:56:53 +0100

    Styling and content revamp

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import {Icon} from 'antd';
+import React from "react";
+import { Icon } from "antd";
+
 
 function Footer() {
     return (
-        <section class="footer">
-           <p>Project Recommend <Icon type="copyright" /> 2020. All rights reserved</p>
+        <section className="footer">
+            <div className="container">
+                <div className="footer-logo">Project Recommend</div>
+                <a href="https://github.com/project-recommend"><Icon type="linkedin" className="footer-links footer-icons" /></a><a href="mailto:projectrecommend@gmail.com"><Icon type="mail" className="footer-links footer-icons" /></a>
+                <p>© 2020. All rights reserved</p>
+            </div>
         </section>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
