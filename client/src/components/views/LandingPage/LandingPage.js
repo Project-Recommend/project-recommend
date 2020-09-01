a1bc9e76a808e11fe6d9be86757f3c964fb933dd
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-09-01 11:43:51 +0100

    Updated landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -25,60 +25,103 @@ function LandingPage() {
                 <div className="container">
                     <div className="row">
                         <div className="col-12 text-center">
-                            <h2>Our Story</h2>
+                            <h2>Our Mission</h2>
+                            <br />
                             <p>
-                                During a four-week project-based learning curriculum, our founders were brought together by the desire to fix a shared problem. Over the four weeks we researched deeper into
-                                the problem domain and affected parties, took on the advice of our peers and potential users, and fine-tuned the solution with the help of mentors. From this, we decided
-                                to create Project Recommend.
+                                Help students and unemployed people find programming-based projects 10x faster. So when they apply for programming roles and opportunties, they have the necessary
+                                skills and experience to show employers.
                             </p>
                         </div>
                     </div>
                     <div className="row justify-content-between about-section">
                         <div className="col-xs-12 col-sm-6 col-lg-4 text-center">
-                            <Icon type="warning" className="big-icons" />
+                            <Icon type="warning" className="small-icons" />
                             <h3>The problem</h3>
                             <p>People spend anywhere from 10 minutes to several hours when searching for programming-based projects to do. This time would be better spend doing the projects.</p>
                         </div>
                         <div className="col-xs-12 col-sm-6 col-lg-4 text-center">
-                            <Icon type="clock-circle" className="big-icons" />
+                            <Icon type="hourglass" className="small-icons" />
                             <h3>Why now</h3>
                             <p>Covid-19 has caused millions to become unemployed and lose internships. Students and professionals need projects to boost their profiles.</p>
                         </div>
                         <div className="col-xs-12 col-sm-12 col-lg-4 text-center">
-                            <Icon type="team" className="big-icons" />
+                            <Icon type="team" className="small-icons" />
                             <h3>Who is affected</h3>
                             <p>
                                 Students thinking of, or currently applying to internships. Unemployed people trying to secure programming roles. Hobbyists who would like to save time on finding
                                 projects to do.
                             </p>
                         </div>
+                        
                     </div>
-                    <div className="row about-section">
+                    <div className="row">
                         <div className="col-12 text-center">
-                            <h2>Our solution</h2>
-                            <p>
-                                Our platform provides a way for you to: find projects quickly and efficiently, track the projects they are doing, and form teams to partake in group projects. Soon, we
-                                will also add a feature that gives users advice on how to talk about projects they have completed in their resumes.
-                            </p>
+                            <br /><br /><h2>Our solution</h2>
                         </div>
                     </div>
+                    <hr className="featurette-divider" />
+                    <div className="row featurette">
+                        <div className="col-md-7">
+                            <h2 className="featurette-heading">
+                                Find projects <span className="text-muted">quickly and effectively.</span>
+                            </h2>
+                            <p className="lead">
+                                Using our platform, you can save 10x as much time when looking for projects. You can either get projects recommended to you by filling in the recommend form, or you can
+                                browse through our project catalog.
+                            </p>
+                        </div>
+                        <div className="col-md-5 text-center">
+                            <Icon type="clock-circle" className="big-icons" />
+                        </div>
+                    </div>
+
+                    <hr className="featurette-divider" />
+
+                    <div className="row featurette">
+                        <div className="col-md-7 order-md-2">
+                            <h2 className="featurette-heading">
+                                Keep track of your <span className="text-muted">favourite projects.</span>
+                            </h2>
+                            <p className="lead">
+                                You can add any projects you decide to do, or plan to do, to your account. This means that you don't lose track of projects you are completing and can also keep
+                                projects in language or topic specific collections.
+                            </p>
+                        </div>
+                        <div className="col-md-5 order-md-1">
+                            <Icon type="pushpin" className="big-icons" />
+                        </div>
+                    </div>
+
+                    <hr className="featurette-divider" />
+
+                    <div className="row featurette">
+                        <div className="col-md-7">
+                            <h2 className="featurette-heading">
+                                Get useful <span className="text-muted">guidance.</span>
+                            </h2>
+                            <p className="lead">Completing projects is not the end of the journey. We offer help on how to talk about projects you have completed in resumes and applications.</p>
+                        </div>
+                        <div className="col-md-5">
+                            <Icon type="info-circle" className="big-icons" />
+                        </div>
+                    </div>
+
+                    <br />
+                    <br />
+                    <br />
                 </div>
             </section>
-            <section className="section gray-bg-2">
+            <section className="section gray-bg-3">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12 col-lg-4" id="feedback-text">
                             <div>
                                 <h6>Let's Chat</h6>
-                                <h2>Get in Contact</h2>
+                                <h2>Give Us Feedback</h2>
                             </div>
                             <p>
                                 We created this platform to help students and professionals. We would love to hear your feedback and experiences using the platform! You can send us a message using
-                                this form or email us at{" "}
-                                <a href="mailto:projectrecommend@gmail.com">
-                                    <strong>projectrecommend@gmail.com</strong>
-                                </a>
-                                .
+                                this form.
                             </p>
                             <p>You can also follow us on LinkedIn, just search for Project Recommend.</p>
                         </div>
